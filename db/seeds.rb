4559c208f7f89cb23be73b358e0c0cea8cd471bd
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-15 16:18:01 +0200

    10 fake articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+puts "Creating 10 fake articles..."
+10.times do
+  article = Article.new(
+    title: Faker::Name.title,
+    content: Faker::Friends.quote,
+    )
+  article.save!
+end
+puts "Done!"
